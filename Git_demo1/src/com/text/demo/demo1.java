dc29e58cccb0ba6a783b804b86a1da08d67b7fa5
Wangxu <504277326@qq.com>
2020-07-18 11:44:15 +0800

    把本地工程添加到git下

---
+++ Git_demo1/src/com/text/demo/demo1.java
@@ -0,0 +1,17 @@
+package com.text.demo;
+
+/**
+ * @ClassName demo1
+ * @Author asus
+ * @Date 2020/7/18 11:18
+ * @Version 1.0
+ * @Function <功能描述>
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Hello Gie Project");
+        }
+
+    }
+}
